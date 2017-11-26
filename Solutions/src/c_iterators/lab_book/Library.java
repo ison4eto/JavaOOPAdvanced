76f5e66d55c7a11a295261589531c0dbec4b6368
iskra <ison4eto635@gmail.com>
2017-11-26 11:02:14 +0200

    public constructors

---
+++ Solutions/src/c_iterators/lab_book/Library.java
@@ -0,0 +1,29 @@
+package lab_book;
+
+import java.util.Iterator;
+
+public class Library<Book> implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibraryIterator();
+    }
+
+    private final class LibraryIterator implements Iterator<Book>{
+        private int counter = 0;
+        @Override
+        public boolean hasNext() {
+            return this.counter<books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[this.counter++];
+        }
+    }
+}
